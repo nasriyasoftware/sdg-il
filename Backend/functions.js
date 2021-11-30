42b9398595398728bb741d6ac1d5105e957cf2ab
Nasriya Software <76511585+nasriyasoftware@users.noreply.github.com>
2021-11-30 10:01:49 +0200

    Create functions.js

---
+++ Backend/functions.js
@@ -0,0 +1,19 @@
+// This is a JavaScript web module (JSW).
+import data from 'wix-data';
+
+export function isValidAuthorID(id) {
+    if (id.length !== 32) { return false }
+    return getAuthor(id).then((author) => { return author ? true : false })
+}
+
+export function getBook(id) {
+    return data.query('Books').eq('_id', id).include('author').limit(1).find({ suppressAuth: true }).then((x) => { return x.items[0] });
+}
+
+export function getAuthor(id) {
+    return data.get('Authors', id, { suppressAuth: true });
+}
+
+export function deleteBook(id) {
+    return data.remove('Books', id, { suppressAuth: true });
+}
